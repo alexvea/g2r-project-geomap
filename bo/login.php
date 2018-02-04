f3268e947a097cacd666a819b9426e910925cae3
alexvea <35368807+alexvea@users.noreply.github.com>
2018-02-04 18:37:59 +0100

    bootstrap page login

--- bo/login.php
+++ bo/login.php
@@ -1,17 +1,30 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Page protégée par mot de passe</title>
-    </head>
-    <body>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Page protégée par mot de passe</title>
+    <!-- Bootstrap core CSS -->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+    <link href="css/bo-style.css" rel="stylesheet">
+    <style media="screen">
+      body {
+        padding-top: 40px;
+        padding-bottom: 40px;
+        background-color: #eee;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="container">
+      <form class="form-signin" action="backoffice.php" method="post">
+        <h2 class="form-signin-heading">Backoffice</h2>
         <p>Veuillez entrer le mot de passe pour accéder au backoffice :</p>
-        <form action="backoffice.php" method="post">
-            <p>
-            <input type="password" name="mot_de_passe" />
-            <input type="submit" value="Valider" />
-            </p>
-        </form>
+        <label for="inputPassword" class="sr-only" ></label>
+        <input type="password" id="inputPassword" name="mot_de_passe" class="form-control" placeholder="Password" required>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Valider</button>
         <p>Cette page est réservée aux gestionnaires du site. Inutile d'insister vous ne trouverez jamais le mot de passe !</p>
-    </body>
+      </form>
+    </div>
+  </body>
 </html>
