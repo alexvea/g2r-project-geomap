e5e38c9351cc4ea24718857f84e4b1cab23671f2
AV <alexvea@gmail.com>
2018-02-06 13:38:30 +0100

    police small filtre react

--- js/react/components/SearchFilter.js
+++ js/react/components/SearchFilter.js
@@ -9,7 +9,7 @@ class SearchFilter extends React.Component {
           <div className={"col-12 col-sm-12 col-md-6 col-lg-4 px-2 py-2 typefiltre"}>
             <div className="custom-control custom-checkbox">
             <input type="checkbox" className="custom-control-input" id={"customCheck"+this.props.division} value={this.props.division} onClick={this.props.action}/>
-            <label className="custom-control-label" for={"customCheck"+this.props.division}>{this.props.division} : <b>{this.props.quantite}</b></label>
+            <label className="custom-control-label" for={"customCheck"+this.props.division}><small>{this.props.division} : <b>{this.props.quantite}</b></small></label>
             </div>
           </div>
     );
